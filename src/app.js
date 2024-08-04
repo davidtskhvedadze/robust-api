6c071f54d5f5d38048d696f70e597af8fbb6a903
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-08-04 17:12:36 -0400

    Add catch all route and error handling middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const loggingMiddleware = require('./middleware/loggingMiddleware');
 const metricsMiddleware = require('./middleware/metricsMiddleware');
 const postRoutes = require('./routes/postRoutes');
 const promClient = require('prom-client');
+const logger = require('../src/utils/logger')
 
 const app = express();
 
@@ -17,6 +18,17 @@ app.use('/v1', postRoutes);
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', promClient.register.contentType);
   res.end(await promClient.register.metrics());
+});
+
+// Catch-all route for handling 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
 });
 
 module.exports = app;
